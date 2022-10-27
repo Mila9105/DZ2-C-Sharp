3db888f70bdfb4c7ab4a4e5d17fc3528545d305a
Mila <anat500@yandex.ru>
2022-10-27 12:36:16 +0200

    решение задач

---
+++ ex 10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int numA=int.Parse(Console.ReadLine()!);
+int SecondDigit(int numA)
+{
+    int SecondDigit=(numA/10)%10;
+    return SecondDigit;
+}
+Console.WriteLine(SecondDigit(numA));
---
+++ ex 15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber=Convert.ToInt32(Console.ReadLine());
+void CheckingTheDayOfTheWeek(int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("выходной день");
+  }
+  else if (dayNumber<1||dayNumber>7) {
+    Console.WriteLine("это не день недели");
+  }
+  else Console.WriteLine("рабочий день");
+}
+CheckingTheDayOfTheWeek(dayNumber);
